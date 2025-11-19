26b0418b46dfb97a6c4ef08912b36f380bad75b5
Liam Durkin <Liam.Durkin@flutteruki.com>
2025-11-19 15:14:59 +0000

    Display JSON in table.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://durkinliam.github.io',
+    site: 'https://durkinliam.github.io/nfl-sim-frontend/',
+    base: '/nfl-sim-frontend/',
 });
